50f9286e05900f38168b8308069c16a3391c1bab
Cristian Franco <cfrancobedoya@gmail.com>
2021-10-12 19:36:17 -0500

    Wiring new database

--- db.js
+++ db.js
@@ -1,30 +1,15 @@
 // By Cristian Franco
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.Promise = global.Promise;
-
-// async function connect(url) {
-//     // mongoose.set('useCreateIndex', true);
-//     await mongoose.connect(url, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     });
-//     console.log('[db] succesfully connected');
-// }
-
-
-
-
-const { MongoClient } = require('mongodb');
+mongoose.Promise = global.Promise;
 
 async function connect(url) {
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        console.log('[db] succesfully connected');
-        const collection = client.db("test").collection("devices");
-        client.close();
+    // mongoose.set('useCreateIndex', true);
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
+    console.log('[db] succesfully connected');
 }
-
 
 module.exports = connect;
